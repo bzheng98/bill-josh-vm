ccc8f195ed5f11ec7c04cc4f8f0eccdc9f0b13fc
y33jung <y33jung@student.cs.uwaterloo.ca>
2017-12-01 10:27:57 -0500

    Add yank files

---
+++ yank.cc
@@ -0,0 +1,28 @@
+#include "yank.h"
+#include "keys.h"
+#include "motionCommand.h"
+#include "range.h"
+#include "commandInfo.h"
+
+void Yank::update(const CommandInfo &c) {
+    if (c.getCommandType() != YANK) return;
+    int motionChar = vm->getChar();
+    if (motionChar == 'y') {
+        yankLines(c.getCount());
+        return;
+    }
+    if (is_escape(motionChar) || !CommandInfo::isMotionCommand((char)motionChar)) return;
+    Range motion = vm->getMotion((char)motionChar);
+}
+
+void Yank::yankLines(int count) {
+    Position begin = fileManager->getCursorPosition();
+    begin.setCol(0);
+    Position end = begin;
+    end.setLine(begin.getLine()+count);
+    yankRange(Range{begin,end});
+}
+
+void Yank::yankRange(const Range &range) {
+    writeToRegister(fileManager->getText(range));
+}
---
+++ yank.h
@@ -0,0 +1,15 @@
+#ifndef YANK_H
+#define YANK_H
+
+#include "writeRegisterCommand.h"
+
+class Yank: public WriteRegisterCommand {
+  public:
+    Yank(Vm *vm, FileManager *fm, RegisterManager *rm):
+        Command{vm,fm,rm},
+        WriteRegisterCommand{vm, fm, rm} {}
+    void update(const CommandInfo &c) override;
+    void yankLines(int count);
+    void yankRange(const Range &range);
+};
+#endif
